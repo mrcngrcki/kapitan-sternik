27f45568ad1e99a251d0977367f1a7b0c3fadc69
Marcin Gorecki <marccin95@gmail.com>
2017-03-25 16:26:01 +0100

    pierwszy commit heroku

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# kapitan-sternik
-heroku
+# new-heroo
---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,23 @@
+package pl.sternik.kk.heroo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet{
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        
+        out.write("Hello Heroku".getBytes());
+        out.flush();
+        out.close();
+    }
+
+}
